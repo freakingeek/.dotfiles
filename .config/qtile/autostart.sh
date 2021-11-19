d3dee6f7d9ec01a7590537c8272f40c8e51887f9
Yasin Silavi <ys.silavi@gmail.com>
2021-11-19 15:42:09 +0330

    update autostart file

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 dunst &
+np-applet &
+light-locker &
 picom --experimental-backends &
-feh --bg-fill --randomize ~/.dotfiles/wallpapers &
+feh --bg-fill ~/.local/share/backgrounds/03.jpg
 gnome-keyring-daemon --start --components=pkcs11,secrets,ssh &
