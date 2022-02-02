db4facd0026f4d9aa6650a1febb10fbfa7f820e7
Yasin Silavi <ys.silavi@gmail.com>
2022-02-02 12:22:36 +0330

    feat: add setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+DOTFILES_PATH=~/.dotfiles
+
+configs=(fontconfig fish)
+locals=(fonts themes backgrounds)
+
+for config in ${configs[@]}; do
+  ln -sf $DOTFILES_PATH/.config/$config ~/.config
+done
+
+for local in ${locals[@]}; do
+  ln -sf $DOTFILES_PATH/.local/$local ~/.local
+done
