d56edb3ff2b1e480b23741e16c7347be3f59f987
Yasin Silavi <ys.silavi@gmail.com>
2022-12-20 14:59:45 +0330

    add toggle-language script

--- scripts/toggle_language.sh
+++ scripts/toggle_language.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-(setxkbmap -query | grep -q "layout:\s\+us") && setxkbmap ir || setxkbmap us
+(setxkbmap -query | grep -q "layout:\s\+us") && setxkbmap ir -option grp_led:caps || setxkbmap us -option grp_led:caps
