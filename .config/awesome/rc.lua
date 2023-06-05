1e58d9fe4159657ed786eeefe0fd9c3d6efa0fa1
Yasin Silavi <ys.silavi@gmail.com>
2023-06-06 01:48:38 +0330

    Change default layout mode in awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -27,6 +27,8 @@ root.keys(keys.globalkeys)
 -- ))
 
 awful.layout.layouts = {
+    awful.layout.suit.spiral,
+    -- awful.layout.suit.spiral.dwindle,
     awful.layout.suit.floating,
     -- awful.layout.suit.tile,
     -- awful.layout.suit.tile.left,
@@ -34,8 +36,6 @@ awful.layout.layouts = {
     -- awful.layout.suit.tile.top,
     -- awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    -- awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier,
