e0062566e222dc6c8fad2660b274fe71d7d5d155
Yasin Silavi <ys.silavi@gmail.com>
2023-06-06 01:48:10 +0330

    Update nvim config and add more plugins

--- .config/nvim/lua/ysilavi/set.lua
+++ .config/nvim/lua/ysilavi/set.lua
@@ -3,9 +3,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.rnu = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -15,7 +15,7 @@ vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
 
-vim.opt.undodir = "/.vim/undodir"
+vim.opt.undodir = "~/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -23,7 +23,7 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 16
 vim.opt.signcolumn = "no"
 vim.opt.isfname:append("@-@")
 
